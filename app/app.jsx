8317ee0be75dad793a67460c75e0d538d76c085c
Nahid <nahidshaiket10300@gmail.com>
2017-02-10 16:29:06 +0600

    browser history

--- app/app.jsx
+++ app/app.jsx
@@ -1,6 +1,6 @@
 var React = require('react');
 var ReactDOM = require('react-dom');
-var {Route, Router, IndexRoute, hashHistory} = require('react-router');
+var {Route, Router, IndexRoute, browserHistory} = require('react-router');
 var Main = require('Main');
 var Countdown = require('Countdown');
 var Timer = require('Timer');
@@ -13,7 +13,7 @@ $(document).foundation();
 require('style!css!sass!applicationStyles');
 
 ReactDOM.render(
-  <Router history={hashHistory}>
+  <Router history={browserHistory}>
     <Route path="/" component={Main}>
       <Route path="countdown" component={Countdown} />
       <IndexRoute component={Timer} />
